d832ea71e6283a1c8d67edde768a6cb17bd27b96
julio <julio@biz.com>
2017-01-31 23:31:54 -0500

    first commit

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+angular.module('starter.configs', [])
+
+.constant("CONFIG", {
+  "FIREBASE_API": 'AIzaSyBNpWCdPs0XUyBIcv38OXE2-bXb9JcLol4',
+  "FIREBASE_AUTH_DOMAIN": 'dev-auth-98fd7.firebaseapp.com',
+  "FIREBASE_DB_URL": 'https://dev-auth-98fd7.firebaseio.com',
+  "FIREBASE_STORAGE": 'dev-auth-98fd7.appspot.com',
+  "MESSAGE_SENDER_ID": '1023928958601'
+});
